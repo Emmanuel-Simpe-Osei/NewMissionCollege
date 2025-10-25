f7d0571f0200c0d6b03406268c5d7da2f9ce6d85
Emmanuel-Simpe-Osei <emmanuelsimpeosei5@gmail.com>
2025-10-25 09:33:32 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        wine: "#8B0000",
+        piction: "#2BA3E6",
+      },
+    },
+  },
+  plugins: [],
+};
